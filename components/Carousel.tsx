b66632dafd0aae798523638e93f5e0031bf44a6d
Matthias <matthias0@skiff.com>
2024-09-23 16:03:55 +0200

    Optimize images and add placeholders

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -13,11 +13,14 @@ export default function Carousel() {
   return (
     <>
       <Image
+        key={currentIndex}
         className="sm:absolute sm:left-1/2 sm:top-[20%] sm:-translate-x-1/2 lg:top-6 -sm:col-span-2 -sm:col-start-2 -sm:row-start-2 -sm:mx-auto -sm:-mt-4"
         src={carouselItems[currentIndex].image.src}
         alt={carouselItems[currentIndex].image.alt}
         width={160}
         height={carouselItems[currentIndex] === carouselItems[0] ? 96 : 120}
+        placeholder="blur"
+        blurDataURL={carouselItems[currentIndex].image.placeholder}
       />
       <div className="w-full sm:absolute sm:bottom-6 sm:left-0 sm:grid sm:grid-cols-6 sm:gap-4 sm:px-6 lg:grid-cols-12 lg:px-8 -sm:col-span-4 -sm:row-start-5">
         <p className="interactive pointer-events-auto sm:col-span-6 sm:text-center lg:col-span-8 lg:col-start-3 -sm:text-sm">
